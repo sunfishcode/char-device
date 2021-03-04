654433a4951ad05d6fc470cc951e98274fe87890
Dan Gohman <dev@sunfishcode.online>
2021-03-04 10:40:21 -0800

    Add tests for `AsyncCharDevice`.

--- tests/null.rs
+++ tests/null.rs
@@ -1,11 +1,26 @@
+#[cfg(feature = "async-std")]
+use char_device::AsyncCharDevice;
 use char_device::CharDevice;
-use std::io::{Read, Write};
 
 #[test]
 fn null() {
+    use std::io::{Read, Write};
+
     let mut char_device = CharDevice::null().unwrap();
     char_device.write_all(b"abcdefg").unwrap();
 
     let mut buf = vec![0_u8; 32];
     assert_eq!(char_device.read(&mut buf).unwrap(), 0);
+}
+
+#[cfg(feature = "async-std")]
+#[async_std::test]
+async fn async_null() {
+    use async_std::io::prelude::{ReadExt, WriteExt};
+
+    let mut char_device = AsyncCharDevice::null().await.unwrap();
+    char_device.write_all(b"abcdefg").await.unwrap();
+
+    let mut buf = vec![0_u8; 32];
+    assert_eq!(char_device.read(&mut buf).await.unwrap(), 0);
 }
--- tests/tty.rs
+++ tests/tty.rs
@@ -1,11 +1,27 @@
 #![cfg(unix)]
 
+#[cfg(feature = "async-std")]
+use char_device::AsyncCharDevice;
 use char_device::CharDevice;
 
 #[test]
 fn tty() {
     // For now, just ensure that we can open this.
     let _tty = match CharDevice::open("/dev/tty") {
+        Ok(tty) => tty,
+        Err(e) => match e.raw_os_error() {
+            // Headless environments sometimes lack /dev/tty.
+            Some(libc::ENXIO) => return,
+            _ => Err(e).unwrap(),
+        },
+    };
+}
+
+#[cfg(feature = "async-std")]
+#[async_std::test]
+async fn async_tty() {
+    // For now, just ensure that we can open this.
+    let _tty = match AsyncCharDevice::open("/dev/tty").await {
         Ok(tty) => tty,
         Err(e) => match e.raw_os_error() {
             // Headless environments sometimes lack /dev/tty.
