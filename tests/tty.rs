8fb14be37274391a245e9b2005960d047c507dc5
Chris Cassano <chris@litprotocol.com>
2024-10-20 20:31:28 -0700

    Revert "Disable async-std and tokio support for now."
    This reverts commit ac36f5175905395ca38c9d8d17e30a907b50ed22.
    
    Tokio now supports IO Safety: https://github.com/tokio-rs/mio/pull/1745

--- tests/tty.rs
+++ tests/tty.rs
@@ -1,14 +1,10 @@
 #![cfg(unix)]
 
-/*
 #[cfg(feature = "async-std")]
 use char_device::AsyncStdCharDevice;
-*/
 use char_device::CharDevice;
-/*
 #[cfg(feature = "tokio")]
 use char_device::TokioCharDevice;
-*/
 
 #[test]
 fn tty() {
@@ -25,7 +21,6 @@ fn tty() {
     };
 }
 
-/*
 #[cfg(feature = "async-std")]
 #[async_std::test]
 async fn async_std_tty() {
@@ -57,4 +52,3 @@ async fn tokio_tty() {
         },
     };
 }
-*/
